c8ed6891ef4228322d3b422483136fd376a9dcd9
mahanmokhtary <mahanmokhtari85@gmail.com>
2025-11-26 14:57:25 +0330

    add flags and srt animation section1step2

---
+++ griffin-css/griffin.js
@@ -0,0 +1,10 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            if (entry.target.classList.contains("section-1-step2-img")) {
+                entry.target.classList.add("section-1-step2-img-animation")
+            }
+        }
+    })
+})
+observer.observe(document.querySelector(".section-1-step2-img"))
