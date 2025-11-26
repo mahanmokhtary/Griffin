eb1f98ae8246de1f3a9b6aaec76d9cfd61ac2ebe
mahanmokhtary <mahanmokhtari85@gmail.com>
2025-11-26 15:26:43 +0330

    dibug animation scroll shine

--- about-us/about-us-script/about-us.js
+++ about-us/about-us-script/about-us.js
@@ -1,19 +1,24 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            if (entry.target.classList.contains("AU-section3-title")) {
-                entry.target.classList.add("AU-section3-title-animation")
+window.addEventListener('load',setAnimations)
+function setAnimations () {
+    const observer = new IntersectionObserver((entries) => {
+        
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                if (entry.target.classList.contains("AU-section3-title")) {
+                    entry.target.classList.add("AU-section3-title-animation")
+                }
+                if (entry.target.classList.contains("AU-section3-info")) {
+                    entry.target.classList.add("AU-section3-info-animation")
+                }
+                if (entry.target.classList.contains("AU-section3-img")) {
+                    entry.target.classList.add("AU-section3-img-animation")
+                }
+    
             }
-            if (entry.target.classList.contains("AU-section3-info")) {
-                entry.target.classList.add("AU-section3-info-animation")
-            }
-            if (entry.target.classList.contains("AU-section3-img")) {
-                entry.target.classList.add("AU-section3-img-animation")
-            }
-
-        }
+        })
     })
-})
-observer.observe(document.querySelector(".AU-section3-title"))
-observer.observe(document.querySelector(".AU-section3-info"))
-observer.observe(document.querySelector(".AU-section3-img"))
+    observer.observe(document.querySelector(".AU-section3-title"))
+    observer.observe(document.querySelector(".AU-section3-info"))
+    observer.observe(document.querySelector(".AU-section3-img"))
+
+}
--- griffin-css/griffin.js
+++ griffin-css/griffin.js
@@ -1,10 +1,13 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            if (entry.target.classList.contains("section-1-step2-img")) {
-                entry.target.classList.add("section-1-step2-img-animation")
+window.addEventListener('load',setAnimations)
+function setAnimations () {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                if (entry.target.classList.contains("section-1-step2-img")) {
+                    entry.target.classList.add("section-1-step2-img-animation")
+                }
             }
-        }
+        })
     })
-})
-observer.observe(document.querySelector(".section-1-step2-img"))
+    observer.observe(document.querySelector(".section-1-step2-img"))
+}
